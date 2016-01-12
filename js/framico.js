70071b00adb8f9ac84eb9939ba37d4aaeeb0b053
Валерий Стрелец <brooons@yandex.ru>
2016-01-12 20:13:47 +0200

    1.5.7

--- js/framico.js
+++ js/framico.js
@@ -34,6 +34,30 @@ $(document).ready(function() {
     });
 });
 
+// Вкладки
+
+$(document).ready(function() {
+    $('.accordion-tabs').children('li').first().children('a').addClass('is-active')
+        .next().addClass('is-open').show();
+
+    $('.accordion-tabs').on('click', 'li > a', function(event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+
+            $('.accordion-tabs .is-open').removeClass('is-open').hide();
+
+            $(this).next().toggleClass('is-open').toggle();
+
+            $('.accordion-tabs').find('.is-active').removeClass('is-active');
+
+            $(this).addClass('is-active');
+
+        } else {
+            event.preventDefault();
+        }
+    });
+});
+
 // Glide.js | Ver: 1.0.5 | http://jedrzejchalubek.com
 
 (function($, window, document, undefined) {
